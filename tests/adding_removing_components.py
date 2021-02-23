0f4fd5db12b43b2dafa15459d383d28d4428f430
Alessio Stalla <alessiostalla@gmail.com>
2021-02-23 09:29:38 +0100

    More add/remove tests

--- tests/adding_removing_components.py
+++ tests/adding_removing_components.py
@@ -1,6 +1,7 @@
 import unittest
 from openpyxl import Workbook
 
+
 class TestAddRemoveSheets(unittest.TestCase):
     def testEmptySheetNames(self):
         wb = Workbook()
@@ -19,12 +20,41 @@ class TestAddRemoveSheets(unittest.TestCase):
         self.assertEqual([], wb.sheetnames)
         wb.close()
 
+
 class TestAddRemoveCells(unittest.TestCase):
+    def testSetCellAddsRowCol(self):
+        wb = Workbook()
+        self.assertEqual(wb.active.max_row, 1)
+        self.assertEqual(wb.active.max_column, 1)
+        wb.active['C3'].value = 12
+        self.assertEqual(wb.active.max_row, 3)
+        self.assertEqual(wb.active.max_column, 3)
+        wb.close()
+
     def testAddRemoveRows(self):
         wb = Workbook()
-        self.assertEqual(wb.active.max_row, 0)
+        self.assertEqual(wb.active.max_row, 1)
+        wb.active['A1'].value = 11
         wb.active.insert_rows(0, 3)
+        self.assertEqual(wb.active.max_row, 4)
+        self.assertEqual(wb.active['A4'].value, 11)
+        wb.active.delete_rows(2, 1)
         self.assertEqual(wb.active.max_row, 3)
-        del wb.active['B']
+        self.assertEqual(wb.active['A3'].value, 11)
+        del wb.active['A3']
         self.assertEqual(wb.active.max_row, 2)
+        wb.close()
+
+    def testAddRemoveCols(self):
+        wb = Workbook()
+        self.assertEqual(wb.active.max_column, 1)
+        wb.active['A1'].value = 11
+        wb.active.insert_cols(0, 3)
+        self.assertEqual(wb.active.max_column, 4)
+        self.assertEqual(wb.active['D1'].value, 11)
+        wb.active.delete_cols(2, 1)
+        self.assertEqual(wb.active.max_column, 3)
+        self.assertEqual(wb.active['C1'].value, 11)
+        del wb.active['C1']
+        self.assertEqual(wb.active.max_column, 2)
         wb.close()
